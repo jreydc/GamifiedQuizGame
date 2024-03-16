0f7f77a7c6d18e2391cdff22f9bfd678b9b9162e
jreydc <jreydc@gmail.com>
2024-03-16 22:51:51 +0800

    created the QuizManager class

---
+++ Assets/_Scripts/Models/Question.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Question
+{
+    public string questionText;
+    public List<Answer> answers;
+    public int correctAnswerIndex;
+}
